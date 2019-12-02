9d23d32305fc92d80c767e610ddc145f585c1883
Michael Nunes <miketnunes@gmail.com>
2019-12-02 12:53:41 -0800

    Add initial server code

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // implement your API here
+// Ok bud :)
+
+//Bring in .env config parser
+require('dotenv').config();
+//Get express module
+const express = require('express');
+//Get CORS module
+const cors = require('cors');
+//Get DB module
+const db = require('./data/db');
+
+//Create server
+const server = express();
+
+//Get / Set port - Use the process env variable PORT, or if missing, use 4000 as fallback.
+const port = process.env.PORT || 4000;
+
+//Use JSON body parser
+server.use(express.json());
+
+//Use CORS
+server.use(cors());
+
+//Initial endpoint
+server.get('/', (req, res) => {
+  res.status(200).json({ message: 'Hello there :)' });
+});
+
+//Start listening
+server.listen(port, () => {
+  console.log(`Server listening on port ${port}...`);
+});

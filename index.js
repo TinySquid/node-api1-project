4cb419ca0a8e48979a1d196cf6cf815f84f4adbf
Michael Nunes <miketnunes@gmail.com>
2019-12-02 13:00:27 -0800

    Add get endpoints for users and user by id

--- index.js
+++ index.js
@@ -27,6 +27,35 @@ server.get('/', (req, res) => {
   res.status(200).json({ message: 'Hello there :)' });
 });
 
+// GET /api/users
+server.get('/api/users', (req, res) => {
+  db.find()
+    .then(users => {
+      if (users) {
+        res.status(200).json(users);
+      } else {
+        res.status(404).json({ message: 'There are no users in the database' });
+      }
+    })
+    .catch(error => {
+      res.status(500).json({ error: 'The users information could not be retrieved.' });
+    });
+});
+
+server.get('/api/users/:id', (req, res) => {
+  db.findById(req.params.id)
+    .then(user => {
+      if (user) {
+        res.status(200).json(user);
+      } else {
+        res.status(404).json({ message: 'The user with the specified ID does not exist.' });
+      }
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
+})
+
 //Start listening
 server.listen(port, () => {
   console.log(`Server listening on port ${port}...`);
